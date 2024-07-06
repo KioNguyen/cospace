7cc2d14b8aebf1a1f57fd7dfa37083171dd577d5
Kio Nguyen <hung.nguyen.tpv@one-line.com>
2024-07-06 18:12:10 +0700

    feat: create layout for dashboard

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import NavBar from "./_component/navbar";
+import OrgSidebar from "./_component/orgsidebar";
+import Sidebar from "./_component/sidebar";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <NavBar />
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
